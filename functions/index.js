84f7281300a908fc5460bb87a8b9df172390fb4b
mohid <usman@mohid.local>
2024-03-28 15:35:59 +0500

    changes

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,23 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-
-
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors"); // Import cors package
 const mongoose = require("mongoose");
 const router = require("./src/routes/router");
 
 const app = express();
 
+// Enable CORS for all origins
+app.use(cors());
 
 app.use(express.json());
 app.use("/", router);
 
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 exports.api = functions.https.onRequest(app);
-
-
-
 
